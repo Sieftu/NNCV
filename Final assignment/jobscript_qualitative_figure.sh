d8674aaba5a156d78855b295e59dcd4cac19adc4
Sieftu <thorabegg@gmail.com>
2026-04-26 12:34:09 +0200

    Congif path update

--- Final assignment/jobscript_qualitative_figure.sh
+++ Final assignment/jobscript_qualitative_figure.sh
@@ -33,7 +33,7 @@ srun apptainer exec --nv \
             --gt     \"\$GT\" \
             --unet-ckpt       checkpoints/unet/best_model_miou.pt \
             --segformer-ckpt  checkpoints/segformer_b2/best_model_miou.pt \
-            --segformer-config segformer_config \
+            --segformer-config /app/segformer_config \
             --output-dir      figures \
             --unet-preprocess starter
     "
